82f74f29be77706df9c5559782b6a1c663af8205
Jagan Ayyakutty <jagan26@gmail.com>
2024-03-24 21:28:01 +0000

    Create and impl Author functions and test

--- src/main/java/com/homeprojects/springjdbc/dao/AuthorDao.java
+++ src/main/java/com/homeprojects/springjdbc/dao/AuthorDao.java
@@ -1,4 +1,11 @@
 package com.homeprojects.springjdbc.dao;
 
+import com.homeprojects.springjdbc.domain.Author;
+
+import java.util.Optional;
+
 public interface AuthorDao {
+    void create(Author author);
+
+    Optional<Author> findOne(long l);
 }
---
+++ src/test/java/com/homeprojects/springjdbc/dao/AuthorDaoImplTests.java
@@ -0,0 +1,48 @@
+package com.homeprojects.springjdbc.dao;
+
+import com.homeprojects.springjdbc.dao.impl.AuthorDaoImpl;
+import com.homeprojects.springjdbc.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import static org.hamcrest.Matchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorDaoImplTests {
+
+    @Mock
+    private JdbcTemplate jdbcTemplate;
+
+    @InjectMocks
+    private AuthorDaoImpl underTest;
+
+    @Test
+    public void testThatCreatesAuthorGeneratesCorrectSql(){
+        Author author = Author.builder()
+                .age(33).id(123456L).name("Dingu").build();
+        underTest.create(author);
+
+        verify(jdbcTemplate).update(
+                eq("INSERT INTO authors (id, name, age) VALUES (?, ?, ?)"),
+                eq(123456L), eq("Dingu"), eq(33)
+                );
+    }
+
+    @Test
+    public void testFindOneGeneratesCorrectSql(){
+        underTest.findOne(123456L);
+
+        verify(jdbcTemplate).query(
+                eq("SELECT id, name, age FROM authors WHERE id = ? LIMIT 1"),
+                ArgumentMatchers.<AuthorDaoImpl.AuthorRowMapper>any(),
+                eq(123456L));
+    }
+}
